9161b111227cfa314f7b79cc8b22a312386927f7
Porke <porke@gazeta.pl>
2017-06-07 22:00:28 +0200

    Extracted historical usage stats controller

--- SSD_Status.UI/Controllers/HistoricalUsageStatsController.cs
+++ SSD_Status.UI/Controllers/HistoricalUsageStatsController.cs
@@ -5,40 +5,96 @@ using SSD_Status.WPF.ViewModels;
 using SSD_Status.WPF.ViewModels.Sources;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Windows.Forms;
+using SSD_Status.WPF.Utilities;
+using SSD_Status.WPF.Persistence;
 
 namespace SSD_Status.WPF.Controllers
 {
     public class HistoricalUsageStatsController
     {
-        private ChartViewModel _viewModel;
+        private HistoricalUsageStatsViewModel _usageViewModel;
+
+        private SmartEntryCsvImporter _smartEntryCsvImporter = new SmartEntryCsvImporter();
+        private SsdDrive _drive = new SsdDrive();
 
         private Dictionary<ChartType, IChartDataSelector> _dataSelectors = new Dictionary<ChartType, IChartDataSelector>();
+        private List<SmartDataEntry> _historicalData = new List<SmartDataEntry>();
 
-        internal HistoricalUsageStatsController(ChartViewModel chartViewModel)
+        public RelayCommand OpenFileCommand { get; private set; }
+        public RelayCommand LoadChartCommand { get; private set; }
+
+        internal HistoricalUsageStatsController(HistoricalUsageStatsViewModel usageViewModel)
         {
-            _viewModel = chartViewModel;
+            _usageViewModel = usageViewModel;
+
+            LoadChartCommand = new RelayCommand(LoadChartCommand_Execute);
+            _usageViewModel.LoadChartCommand = LoadChartCommand;
 
             _dataSelectors.Add(ChartType.None, new NoneSelector());
             _dataSelectors.Add(ChartType.HostWrittenGbInTime, new HostWritesSelector());
             _dataSelectors.Add(ChartType.HostWrittenGbPerPowerOnHoursInTime, new HostWritesPerHoursOnSelector());
             _dataSelectors.Add(ChartType.WearLevellingInTime, new WearLevellingSelector());
             _dataSelectors.Add(ChartType.PowerOnHoursInTime, new PowerOnHoursSelector());
+
+            OpenFileCommand = new RelayCommand(OpenFileCommand_Execute);
+            _usageViewModel.OpenFileCommand = OpenFileCommand;
         }
 
-        internal void SelectChartData(IReadOnlyList<SmartDataEntry> records, ChartTypeViewModel chartTypeVm)
+        private void LoadChartCommand_Execute(object chartType)
         {
-            _viewModel.SeriesValues.Clear();
-            _viewModel.Timestamps.Clear();
+            var chartTypeVm = chartType as ChartTypeViewModel;            
+            UpdateChart(_historicalData, chartTypeVm);
+        }
+
+        private void OpenFileCommand_Execute(object obj)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _usageViewModel.SourceDataFile = openFileDialog.FileName;
+                    _historicalData.Clear();
+
+                    _historicalData.AddRange(_smartEntryCsvImporter.ImportSmartEntries(openFileDialog.FileName));
+                    CalculateLifeEstimates();
+                }
+            }
+        }
+
+        private void CalculateLifeEstimates()
+        {
+            _historicalData = EntryAggregator.AggregateEntriesByDay(_historicalData.AsReadOnly()).ToList();
+
+            var firstEntry = _historicalData.First();
+            var lastEntry = _historicalData.Last();
+            double usagePerDay = _drive.CalculateHostWrittenGbPerDay(firstEntry, lastEntry);
+            double hourUsagePerDay = _drive.CalculatePowerOnHoursPerDay(firstEntry, lastEntry);
+            double gigabytesPerHour = _drive.CalculateHostWrittenGbPerPowerOnHours(firstEntry, lastEntry);
+            double wearPerDay = _drive.CalculateWearLevellingPerDay(firstEntry, lastEntry);
+
+            _usageViewModel.LifeEstimates.Clear();
+            _usageViewModel.LifeEstimates.Add($"Usage per day: {usagePerDay.ToString("0.##", CultureInfo.InvariantCulture)} GB");
+            _usageViewModel.LifeEstimates.Add($"Hour usage per day: {hourUsagePerDay.ToString("0.##", CultureInfo.InvariantCulture)} h");
+            _usageViewModel.LifeEstimates.Add($"Gigabytes per usage hour: {gigabytesPerHour.ToString("0.##", CultureInfo.InvariantCulture)} GB");
+            _usageViewModel.LifeEstimates.Add($"Wear per day: {wearPerDay.ToString("0.####", CultureInfo.InvariantCulture)}");
+        }
+
+        internal void UpdateChart(IReadOnlyList<SmartDataEntry> records, ChartTypeViewModel chartTypeVm)
+        {
+            var chartViewModel = _usageViewModel.ChartViewModel;
+            chartViewModel.SeriesValues.Clear();
+            chartViewModel.Timestamps.Clear();
 
             IChartDataSelector selector = _dataSelectors[chartTypeVm.Type];
-            _viewModel.YAxisTitle = selector.YAxisDescription;
-            _viewModel.SeriesTitle = chartTypeVm.Description;
+            chartViewModel.YAxisTitle = selector.YAxisDescription;
+            chartViewModel.SeriesTitle = chartTypeVm.Description;
 
             var selectedData = selector.SelectData(records);
-            _viewModel.Minimum = selectedData.Any() ? selectedData.Select(x => x.Value).Min() : 0;
-            _viewModel.Maximum = selectedData.Any() ? selectedData.Select(x => x.Value).Max() : 1;
-            _viewModel.Timestamps.AddRange(selectedData.Select(x => x.Key.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)));
-            _viewModel.SeriesValues.AddRange(selectedData.Select(x => x.Value));
+            chartViewModel.Minimum = selectedData.Any() ? selectedData.Select(x => x.Value).Min() : 0;
+            chartViewModel.Maximum = selectedData.Any() ? selectedData.Select(x => x.Value).Max() : 1;
+            chartViewModel.Timestamps.AddRange(selectedData.Select(x => x.Key.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)));
+            chartViewModel.SeriesValues.AddRange(selectedData.Select(x => x.Value));
         }
     }
 }
--- SSD_Status.UI/Controllers/MainController.cs
+++ SSD_Status.UI/Controllers/MainController.cs
@@ -17,72 +17,22 @@ namespace SSD_Status.WPF.Controllers
         private RawValueInfoController _rawValueInfoController;
 
         private SsdDrive _drive = new SsdDrive();
-        private List<SmartDataEntry> _historicalData = new List<SmartDataEntry>();
         private List<SmartDataEntry> _realTimeData = new List<SmartDataEntry>();
-        private Timer _realTimeModeTimer = new Timer();
-        private SmartEntryCsvImporter _smartEntryCsvImporter = new SmartEntryCsvImporter();
-
-        public RelayCommand OpenFileCommand { get; private set; }        
-        public RelayCommand LoadChartCommand { get; private set; }
+        private Timer _realTimeModeTimer = new Timer();        
+                
         public RelayCommand ToggleMonitoringCommand { get; private set; }
         public RelayCommand ExportReadingsCommand { get; private set; }
 
         public MainController(MainViewModel viewModel)
         {
             _viewModel = viewModel;
-            _historicalUsageChartController = new HistoricalUsageStatsController(_viewModel.UsageStatsInfo.ChartViewModel);
+            _historicalUsageChartController = new HistoricalUsageStatsController(_viewModel.UsageStatsInfo);
             _rawValueInfoController = new RawValueInfoController(_viewModel.RawValueInfo);
-
-            OpenFileCommand = new RelayCommand(OpenFileCommand_Execute);            
-            LoadChartCommand = new RelayCommand(LoadChartCommand_Execute);
-            _viewModel.UsageStatsInfo.OpenFileCommand = OpenFileCommand;
-            _viewModel.UsageStatsInfo.LoadChartCommand = LoadChartCommand;
-
+                        
             ToggleMonitoringCommand = new RelayCommand(ToggleMonitoringCommand_Execute);
             ExportReadingsCommand = new RelayCommand(ExportReadingsCommand_Execute);
             _viewModel.RealTimeUsageInfo.ToggleMonitoringCommand = ToggleMonitoringCommand;
             _viewModel.RealTimeUsageInfo.ExportReadingsCommand = ExportReadingsCommand;            
-        }
-
-        private void OpenFileCommand_Execute(object obj)
-        {
-            using (var openFileDialog = new OpenFileDialog())
-            { 
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    _viewModel.UsageStatsInfo.SourceDataFile = openFileDialog.FileName;
-                    _historicalData.Clear();
-
-                    _historicalData.AddRange(_smartEntryCsvImporter.ImportSmartEntries(openFileDialog.FileName));                    
-                    CalculateLifeEstimates();
-                }
-            }
-        }
-
-        private void LoadChartCommand_Execute(object chartType)
-        {
-            var chartTypeVm = chartType as ChartTypeViewModel;            
-            IEnumerable<KeyValuePair<DateTime, double>> records = new List<KeyValuePair<DateTime, double>>();
-
-            _historicalUsageChartController.SelectChartData(_historicalData, chartTypeVm);            
-        }
-
-        private void CalculateLifeEstimates()
-        {
-            _historicalData = EntryAggregator.AggregateEntriesByDay(_historicalData.AsReadOnly()).ToList();
-
-            var firstEntry = _historicalData.First();
-            var lastEntry = _historicalData.Last();            
-            double usagePerDay = _drive.CalculateHostWrittenGbPerDay(firstEntry, lastEntry);
-            double hourUsagePerDay = _drive.CalculatePowerOnHoursPerDay(firstEntry, lastEntry);
-            double gigabytesPerHour = _drive.CalculateHostWrittenGbPerPowerOnHours(firstEntry, lastEntry);
-            double wearPerDay = _drive.CalculateWearLevellingPerDay(firstEntry, lastEntry);
-
-            _viewModel.UsageStatsInfo.LifeEstimates.Clear();
-            _viewModel.UsageStatsInfo.LifeEstimates.Add($"Usage per day: {usagePerDay.ToString("0.##", CultureInfo.InvariantCulture)} GB");
-            _viewModel.UsageStatsInfo.LifeEstimates.Add($"Hour usage per day: {hourUsagePerDay.ToString("0.##", CultureInfo.InvariantCulture)} h");
-            _viewModel.UsageStatsInfo.LifeEstimates.Add($"Gigabytes per usage hour: {gigabytesPerHour.ToString("0.##", CultureInfo.InvariantCulture)} GB");
-            _viewModel.UsageStatsInfo.LifeEstimates.Add($"Wear per day: {wearPerDay.ToString("0.####", CultureInfo.InvariantCulture)}");            
         }
 
         private void ToggleMonitoringCommand_Execute(object obj)
